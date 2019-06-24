e2ed69c46d6afd8a2cb290d6a176b6cefeea7783
Jared <jaredshupert19@gmail.com>
2019-06-24 13:35:46 -0500

    iPad and iPhone layouts

---
+++ dev-profile/Controller/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  dev-profile
+//
+//  Created by Jared Shupert on 6/23/19.
+//  Copyright © 2019 Jared Shupert. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var devopsLogo: UIImageView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ dev-profile/Model/RoundedDevslopesLogo.swift
@@ -0,0 +1,9 @@
+//
+//  RoundedDevslopesLogo.swift
+//  dev-profile
+//
+//  Created by Jared Shupert on 6/23/19.
+//  Copyright © 2019 Jared Shupert. All rights reserved.
+//
+
+
